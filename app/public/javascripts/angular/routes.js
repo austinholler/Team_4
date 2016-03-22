adb4687c2e5ddece35dd414d0d006749d463e72f
unknown <matt.bubernak@gmail.com>
2016-03-22 18:21:54 -0400

    added about page

--- app/public/javascripts/angular/routes.js
+++ app/public/javascripts/angular/routes.js
@@ -6,7 +6,7 @@
 // =================================================
 // 3/1    MB       File Creation
 // 3/2    MB       Router no longer explodes the page.
-
+// 3/22   MB       Added route for index and about
 
 angular.module('routes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
@@ -14,9 +14,17 @@ angular.module('routes', []).config(['$routeProvider', '$locationProvider', func
             templateUrl: 'pages/splash.html',
             controller: 'MainController'
         })
+        .when('/index.html', {
+            templateUrl: 'pages/splash.html',
+            controller: 'MainController'
+        })
         .when('/city.html', {
             templateUrl: 'pages/city.html',
             controller: 'CityController'
+        })
+        .when('/about.html', {
+            templateUrl: 'pages/about.html',
+            controller: 'MainController'
         });
     $locationProvider.html5Mode(true);
 
---
+++ app/public/javascripts/main.js
@@ -0,0 +1,12 @@
+// Edit History
+// Date    Author   Description
+// =================================================
+// 3/22    MB       Default javascript for app
+
+$(document).ready(function(){
+    $('.collapsible').collapsible({
+        accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+    });
+    console.log('set');
+});
+console.log('wattup');

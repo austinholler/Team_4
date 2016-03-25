f9732eecade8c5d11fff367bbe900a78a0b9eb32
unknown <matt.bubernak@gmail.com>
2016-03-24 21:12:50 -0600

    added font-awesome for icons, and some updates to the dbHandler for angular. Works for grabbing basic data from dynamo

--- app/public/javascripts/angular/app.js
+++ app/public/javascripts/angular/app.js
@@ -6,6 +6,7 @@
 // =================================================
 // 3/1    MB       File Creation
 // 3/2    MB       Added ngRoute
+// 3/24   MB       Added DBService
 
 var app = angular.module("app",
     ['ngRoute','routes','MainCtrl','CityCtrl','DBService']);
--- app/public/javascripts/angular/services/DBService.js
+++ app/public/javascripts/angular/services/DBService.js
@@ -1,18 +1,47 @@
 /**
  * Created by socce on 3/24/2016.
  */
+// Edit History
+// Date    Author   Description
+// =================================================
+// 3/24    MB       File Creation
+
+
+var StateEnum = {Loaded:1, Loading:2, NotLoaded:3}
+
 angular.module('DBService', []).service('DatabaseService', function($http,$q) {
-    this.data = []
+    this.data = [];
+    this.state = StateEnum.NotLoaded
 
-    this.init = function() {
-        this.data.push('test1');
-        this.data.push('test2');
-        console.log('inside init')
-        return "initd"
+    this.init = function(callback) {
+        console.log('inside init');
+
+        // Change state to loading...
+        this.state = StateEnum.Loading;
+
+        // Put in some sample data for now...
+        $http.get('http://localhost:3000/api/cities/boulder/co').then(function (data) {
+            // Update data
+            this.data = data;
+            // Update state to loaded.
+            this.state = StateEnum.Loaded;
+            // Call the callback.
+            callback(null, data)
+        })
+
     }
-    this.getData = function(){
-        console.log('inside getData')
+    this.getData = function(callback){
+        console.log('inside getData');
 
-        return data
+        // If we haven't yet loaded the data into the service.
+        if (this.state == StateEnum.NotLoaded)
+        {
+            this.init(callback)
+        }
+
+        // If the data has already been loaded into the service.
+        if (this.state == StateEnum.Loaded) {
+            callback(null,this.data)
+        }
     }
 })

5349a07f0eaf41dddaf2cd24b53db8d5c180ebb9
unknown <matt.bubernak@gmail.com>
2016-03-24 21:12:50 -0600

    added font-awesome for icons, and some updates to the dbHandler for angular. Works for grabbing basic data from dynamo

--- app/public/javascripts/angular/controllers/CityCtrl.js
+++ app/public/javascripts/angular/controllers/CityCtrl.js
@@ -4,11 +4,24 @@
 // Edit History
 // Date    Author   Description
 // ==========================================================
-// 3/24    MB       File Creation
+// 3/24    MB       File Creation, dynamic loading of city
+//                  data by using the dbHandler.
 
 angular.module('CityCtrl', []).controller('CityController', ['$scope','DatabaseService','$routeParams',function($scope,DatabaseService,$routeParams) {
+    // City Name
     $scope.cityName = $routeParams.cityName;
+
+    // City State
     $scope.cityState = $routeParams.cityState;
-    $scope.test = "I am the city Controller";
-    DatabaseService.init();
+
+    // Data for the city, comes from DBService
+    $scope.dataLoaded = false;
+
+    $scope.cityData = null;
+
+    DatabaseService.getData(function(err,data) {
+        $scope.dataLoaded = true;
+        $scope.cityData = data;
+    })
+
 }]);

7e540e8d6066ee7a3ae28a94d4de065c70fca50d
Tushar Singh <tushar.s.2007@gmail.com>
2016-03-27 14:18:08 -0600

    Updated dynamo_script.py

--- scripts/db_scripts/dynamo_script.py
+++ scripts/db_scripts/dynamo_script.py
@@ -26,18 +26,21 @@ def printProgress (iteration, total, prefix = '', suffix = '', decimals = 2, bar
 REGION = "us-west-2"
 conn = dynamodb2.connect_to_region(
     REGION,
-    aws_access_key_id='xxxxxxxxxxxxx',
-    aws_secret_access_key='xxxxxxxxxxxxxxxx',
+    aws_access_key_id='xxxxxxxxxx',
+    aws_secret_access_key='xxxxxxxxxxxxxxxxxx',
 )
 
 #DynamoDB Table
+if len(sys.argv) < 3:
+    sys.exit('Usage: %s CityCode Filepath' % sys.argv[0])
+
 topics = Table(
-    'topics_DEN',
+    'topics_' + sys.argv[1],
     connection=conn
 )
 
 #Input json file name
-js = sys.argv[1]
+js = sys.argv[2]
 
 #Loading data in Tables
 with open(js) as json_file :
@@ -47,7 +50,7 @@ with open(js) as json_file :
 	  printProgress(i, size, prefix = 'Data', suffix = 'Complete', barLength = 50)
           for date in data:
 	     try :
-                for topic in data[date] : 
+                for topic in data[date] :
                 	topics.put_item(data={'Id': data[date][topic]["topic_id"]  ,'Name' : topic , 'Category' : data[date][topic]['category'] ,'Date' : date, 'Score' : Decimal(str(data[date][topic]["score"])) } )
 	                #print data[date][topic]["topic_id"]  , topic ,data[date][topic]["category"] ,  date
 	     except : 

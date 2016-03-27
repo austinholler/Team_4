7527185df3bfe35d414fcbc5965ca85c42fd9d7b
Tushar Singh <tushar.s.2007@gmail.com>
2016-03-27 13:32:26 -0600

    Updated dynamo_script.py with Progress Bar

--- scripts/db_scripts/dynamo_script.py
+++ scripts/db_scripts/dynamo_script.py
@@ -2,26 +2,55 @@ import os, json
 from boto import dynamodb2
 from boto.dynamodb2.table import Table
 from decimal import Decimal
+import sys
 
+# Print iterations progress
+def printProgress (iteration, total, prefix = '', suffix = '', decimals = 2, barLength = 100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iterations  - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+    """
+    filledLength    = int(round(barLength * iteration / float(total)))
+    percents        = round(100.00 * (iteration / float(total)), decimals)
+    bar             = '#' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('%s [%s] %s%s %s\r' % (prefix, bar, percents, '%', suffix)),
+    sys.stdout.flush()
+    if iteration == total:
+        print("\n")
+
+#AWS Connection String
 REGION = "us-west-2"
 conn = dynamodb2.connect_to_region(
     REGION,
     aws_access_key_id='xxxxxxxxxxxxx',
-    aws_secret_access_key='xxxxxxxxxxxxxxxxxxxx',
+    aws_secret_access_key='xxxxxxxxxxxxxxxx',
 )
+
+#DynamoDB Table
 topics = Table(
     'topics_DEN',
     connection=conn
 )
-path_to_json = '/path/to/json_dir/'
-json_files = [pos_json for pos_json in os.listdir(path_to_json) if pos_json.endswith('.json')]
-f = 0
-for js in json_files :
-      f += 1
-      with open(os.path.join(path_to_json, js)) as json_file :
+
+#Input json file name
+js = sys.argv[1]
+
+#Loading data in Tables
+with open(js) as json_file :
           data = json.load(json_file)
+	  size = len(data)
+	  i = 0
+	  printProgress(i, size, prefix = 'Data', suffix = 'Complete', barLength = 50)
           for date in data:
+	     try :
                 for topic in data[date] : 
                 	topics.put_item(data={'Id': data[date][topic]["topic_id"]  ,'Name' : topic , 'Category' : data[date][topic]['category'] ,'Date' : date, 'Score' : Decimal(str(data[date][topic]["score"])) } )
 	                #print data[date][topic]["topic_id"]  , topic ,data[date][topic]["category"] ,  date
-      print 'Files Completed:' + str(f*100.0/len(json_files))
+	     except : 
+		pass
+	     printProgress(i, size, prefix = 'Data', suffix = 'Complete', barLength = 50)
+             i += 1

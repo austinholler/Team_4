41210bd295a5a3b184334f69783fdf03fa834cb5
unknown <matt.bubernak@gmail.com>
2016-04-10 12:27:47 -0600

    secret template and new gitignore

---
+++ app/secretTEMPLATE.js
@@ -0,0 +1,7 @@
+// To run locally you need a copy of this file, that is called secret.js, with the correct info.
+// DO NOT modify this file or commit it.
+var Secret = new Object();
+Secret.accessKey = '';
+Secret.secretAccessKey = '';
+Secret.cacheURL = ''
+module.exports = Secret;

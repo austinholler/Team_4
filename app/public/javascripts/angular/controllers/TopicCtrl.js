4ae5e6cef052805946715a3cb4ea36c43c993b25
unknown <matt.bubernak@gmail.com>
2016-04-14 18:23:15 -0600

    added topic page

---
+++ app/public/javascripts/angular/controllers/TopicCtrl.js
@@ -0,0 +1,19 @@
+// TopicCtrl.js
+// Created by: Matt Bubernak
+
+// Edit History
+// Date    Author   Description
+// ==========================================================
+// 4/14    MB       File creation.
+
+angular.module('TopicCtrl', []).controller('TopicController', ['$scope','DatabaseService','$routeParams',
+    function($scope,DatabaseService,$routeParams) {
+
+    }]);
+
+
+
+
+
+
+

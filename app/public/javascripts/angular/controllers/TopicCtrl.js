bb20c79b8176e5cf4343d0b3eeb30059f15f3fc6
unknown <matt.bubernak@gmail.com>
2016-04-17 22:37:59 -0600

    additional rankings

--- app/public/javascripts/angular/controllers/TopicCtrl.js
+++ app/public/javascripts/angular/controllers/TopicCtrl.js
@@ -8,39 +8,61 @@
 // 4/14    MB       Now loads cache and displays rank.
 // 4/17    MB       Supports topic rank for current day.
 // 4/17    MB       Updated time to reflect UTC offset.
+// 4/17    MB       Additional rankings
 
 angular.module('TopicCtrl', []).controller('TopicController', ['$scope','DatabaseService','$routeParams',
 function($scope,DatabaseService,$routeParams) {
     // City Name
     $scope.topic = $routeParams.topic;
-    $scope.globalRank = "Calcuating...";
+    $scope.globalRankDay = "Calcuating...";
+    $scope.globalRankMonth = "Calcuating...";
+    $scope.globalRankYear = "Calcuating...";
 
     // Topic Cache request
-    $scope.topicCacheDataArr = null;
-    var topicCacheDataMap = null;
+    $scope.topicCacheArrToday = null;
+    var topicCacheMapToday = null;
+    $scope.topicCacheArrMonth = null;
+    var topicCacheMapMonth = null;
+    $scope.topicCacheArrYear = null;
+    var topicCacheMapYear = null;
 
     // Async call to load cache data.
     var today = new Date();
     today.setHours(today.getHours()-6); // UTC Offset for MT
     var todayString = today.toISOString().slice(0,10).replace(/-/g,"");
     DatabaseService.getData("cache",{'code':'ALL','type':'top','time':todayString},function(err,data) {
-        topicCacheDataMap = data.data;
-        $scope.topicCacheDataArr = Object.keys(topicCacheDataMap).map(function(key) {
-            return {"topic" : key, "score" : Number(topicCacheDataMap[key]), "url" : "topic/" + key}
+        topicCacheMapToday = data.data;
+        $scope.topicCacheArrToday = Object.keys(topicCacheMapToday).map(function(key) {
+            return {"topic" : key, "score" : Number(topicCacheMapToday[key]), "url" : "topic/" + key}
         })
-        $scope.globalRank = getRank(data);
+        $scope.globalRankDay = getRank(topicCacheMapToday,$scope.topicCacheArrToday);
+    });
+
+    var monthString = today.toISOString().slice(0,10).replace(/-/g,"").slice(0,-2);
+    DatabaseService.getData("cache",{'code':'ALL','type':'top','time':monthString},function(err,data) {
+        topicCacheMapMonth = data.data;
+        $scope.topicCacheArrMonth = Object.keys(topicCacheMapMonth).map(function(key) {
+            return {"topic" : key, "score" : Number(topicCacheMapMonth[key]), "url" : "topic/" + key}
+        })
+        $scope.globalRankMonth = getRank(topicCacheMapMonth,$scope.topicCacheArrMonth);
+    });
+
+    var yearString = today.toISOString().slice(0,10).replace(/-/g,"").slice(0,-4);
+    DatabaseService.getData("cache",{'code':'ALL','type':'top','time':yearString},function(err,data) {
+        topicCacheMapYear = data.data;
+        $scope.topicCacheArrYear = Object.keys(topicCacheMapYear).map(function(key) {
+            return {"topic" : key, "score" : Number(topicCacheMapYear[key]), "url" : "topic/" + key}
+        })
+        $scope.globalRankYear = getRank(topicCacheMapYear,$scope.topicCacheArrYear);
     });
 
 
-    function getRank() {
-        if ($scope.topic in topicCacheDataMap) {
+    function getRank(topicMap,topicArr) {
+        if ($scope.topic in topicMap) {
             var rank = 1;
-            var curScore = topicCacheDataMap[$scope.topic]
-            //console.log(topicCacheDataMap);
-            //console.log($scope.topic);
-            for (var x in $scope.topicCacheDataArr) {
-                //console.log($scope.topicCacheDataArr[x]);
-                if ($scope.topicCacheDataArr[x].score > curScore) rank++;
+            var curScore = topicMap[$scope.topic]
+            for (var x in topicArr) {
+                if (topicArr[x].score > curScore) rank++;
             }
             return rank;
         }
